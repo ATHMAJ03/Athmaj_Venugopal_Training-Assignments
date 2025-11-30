66bb84aa9110b415ec1879fd20aaefeacf14210b
ATHMAJ03 <118753139+ATHMAJ03@users.noreply.github.com>
2025-11-30 19:58:12 +0530

    Add files via upload

---
+++ frequencyOfEachElement.java
@@ -0,0 +1,41 @@
+package javaDemo.mul;
+
+import java.util.*;
+
+public class frequencyOfEachElement {
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter array elements:");
+        for(int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        boolean[] visited = new boolean[n];
+
+        System.out.println("\nFrequency of each element:");
+        for(int i = 0; i < n; i++) {
+            if(visited[i] == true)
+                continue;
+
+            int count = 1;
+            for(int j = i + 1; j < n; j++) {
+                if(arr[i] == arr[j]) {
+                    visited[j] = true;
+                    count++;
+                }
+            }
+
+            System.out.println(arr[i] + " → " + count + " times");
+        }
+
+        sc.close();
+    }
+}
+
